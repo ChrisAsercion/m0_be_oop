5085035d78f0c530f82a3f6643d4d59661b6b33b
Chris Asercion <chris.asercion@gmail.com>
2023-04-02 16:43:36 -0600

    class technique

--- oop.rb
+++ oop.rb
@@ -3,14 +3,19 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 class Unicorn
-    attr_reader :name
-    def initialize(name, color)
+    attr_reader :name, :statement
+    def initialize(name, color, statement)
         @name = name
         @color = silver
+        @statement = statement
     end
     
-    def say
-        p 
+    def say(statement)
+        p "*~*" @statement "*~*"
+    end
+end
+
+Unicorn1 = Unicorn.new("Sasha", "gold")
 
 
 
@@ -26,10 +31,13 @@ class Vampires
         @pet = pet
         @thirsty = true
     end
+    def drink(thirsty)
+        @thirsty = false
+    end
 end
 
 Lord1 = Vampires.new("Vlad", "Butler")
-
+Lord1.drink(true)
 
 
 class Vampires
@@ -59,7 +67,28 @@ end
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
 class Dragon
-    attr_reader :
+    attr_reader :name, :rider, :color, :is_hungry
+    def initialize(name, rider, color, is_hungry)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+    end
+
+    def eat(is_hungry)
+        if is_hungry >= 4
+            p @is_hungry = false
+        else
+            p is_hungry = true
+        end
+    end
+end 
+    
+Egg1 = Dragon.new("Volknir", "Eragorn", "blue")
+p Egg1
+Egg1.eat(5)
+p Egg1
+        
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -69,3 +98,15 @@ class Dragon
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+    attr_reader :name, :disposition, :age
+    def initalize(name, disposition, age)
+        @name = name
+        @disposition = disposition
+        @age = 0
+    end
+
+    def celebrate_birthday(age)
+
+    
